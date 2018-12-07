e07c950f7e10f026fe9c2a67a436ef6d4dc94ef8
Javier González Ovalle <jagonzalezo@falabella.cl>
2018-12-07 19:22:25 -0300

    Initial Commit

---
+++ CodeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeTest
+//
+//  Created by Javier Cristóbal González Ovalle on 12/7/18.
+//  Copyright © 2018 BairesDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
